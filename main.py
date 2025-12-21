7804924af45eaf6f42efc27f01c1e74c806c07ca
MohammedKamalAlsyd <MohammedKamalAlsyd@gmail.com>
2025-12-22 00:11:33 +0200

    Change Localhosts to correct hostname

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from smart-cities!")
+import os
+from confluent_kafka import SerializingProducer
+import simplejson as json
 
 
-if __name__ == "__main__":
-    main()
+
+# Air quality (PM2.5, CO₂, NO₂), temperature, humidity, noise levels, UV index.
